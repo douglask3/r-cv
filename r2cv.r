a412b17d4f060df2b58f6e61d6ebe48b070db8d9
douglask3 - Reading <douglas.i.kelley@gmail.com>
2016-01-29 15:30:53 +0000

    rearranging where outputs go

--- r2cv.r
+++ r2cv.r
@@ -1,6 +1,6 @@
 r2cv <- function(Top = NULL, Name = NULL, Contact = NULL, AdditionalSection = NULL,
                     Footer = NULL,
-                 file = "example-long.r", template = "ThomasHardy", outPath = "docs") {
+                 file = "example-long.r", template = "ThomasHardy", outPath = "output") {
 
     ###############################################################
     ## Cfg                                                       ##
@@ -12,29 +12,40 @@ r2cv <- function(Top = NULL, Name = NULL, Contact = NULL, AdditionalSection = NU
     source("../webpageGenerator/libs/googleScholarGrab/MakePublicationDocument.list.r")
     source("../webpageGenerator/libs/googleScholarGrab/MakePublicationDocument.Rlist.r")
 
+    if (!is.null(file)) source(file, local = TRUE)
+
     ## File paths
     template = paste('template', template, sep = '/')
     index    = paste(template, 'index.r', sep = '/')
 
-    cps2     = list.files(template)
+
+
+
+    outFile    = paste(c(head(strsplit(file, '.', fixed = TRUE)[[1]],-1),'html'), collapse = '.')
+
+    cpInFiles  = c(cssFile  = paste(template, 'style.css', sep = '/'),
+                  imageFile = Name[4])
+
+    Name[4]    = tail(strsplit(Name[4], '/')[[1]],1)
+
+    cpOutFiles = c(cssFile   = 'style.css',
+                   imageFile = Name[4])
 
     if (outPath != "") {
-        out  = paste(outPath , file, sep = '/')
-        cps1 = paste(template, cps2, sep = '/')
-        cps2 = paste(outPath , cps2, sep = '/')
+        makeDir(outPath)
+        outFile    = paste(outPath , outFile, sep = '/')
+        cpOutFiles = paste(outPath, cpOutFiles, sep = '/')
     }
 
 
-    ## Open
-    if (!is.null(file)) source(file, local = TRUE)
     Credits = 'Made using r2cv R package - <a href = "http://github.com/douglask3/r-cv", target = "_blank"> github.com/douglask3/r-cv </a>'
 
     source(index, local = TRUE)
     doc = paste(doc, collapse = "")
 
     ## Outputs
-    cat(doc, file = 'docs/index.html')
+    cat(doc, file = outFile)
 
-    mapply(file.copy, cps1, cps2, overwrite= TRUE)
+    mapply(file.copy, cpInFiles, cpOutFiles, overwrite= TRUE)
 
 }
