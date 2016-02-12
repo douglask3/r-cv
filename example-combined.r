07c426e33604dc538c2ca9e8d4d99cdeb1a565bb
douglask3 - Reading <douglas.i.kelley@gmail.com>
2016-02-12 15:11:00 +0000

    referee email on summary

--- example-combined.r
+++ example-combined.r
@@ -8,9 +8,17 @@ file.copy(files, "temp/", overwrite = TRUE)
 files = paste("temp/", files, sep = '')
 
 
+line = 'Referee = list(Name = "References",
+              c("Prof. Sandy Harrison" = "http://www.reading.ac.uk/s-p-harrison.aspx",
+                "s.p.harrison@reading.ac.uk"),
 
-line = 'Referee = c("References",
-                    "See bottom of extended CV")
+              c("Prof. Colin Prentice" = "http://www.imperial.ac.uk/people/c.prentice",
+                "c.prentice@imperial.ac.uk"),
+
+              c("Prof. Belinda Medlyn" = "https://bmedlyn.wordpress.com/",
+                "b.medlyn@westernsydney.edu.au"),
+
+              c("","Full contact information at end of Extended CV"))
         Name[3] = "Summary"
         AdditionalSection = list(Qualifications, Employment, Publications, Awards,  Research, Skills, Referee)
         Top = NULL'
