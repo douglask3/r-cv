dd664f770d9a7f87271bee2c7ba1eab762b6984e
douglask3 - Reading <douglas.i.kelley@gmail.com>
2016-02-23 10:44:44 +0000

    A cover letter

---
+++ example-coverLetter.r
@@ -0,0 +1,53 @@
+###############################################################
+## Basic Infomation                                          ##
+###############################################################
+###############################################################
+## Basic Infomation                                          ##
+###############################################################
+Name    = c("Douglas Kelley", " ",
+            'Re: Postdoctoral Researcher in African Climate Science')
+
+Contact = c(email = "douglas.i.kelley@gmail.com",
+            phone = "+44 (0) 7936 726 819",
+            web   = "douglask3.github.io")
+
+Authors = c("DI Kelley", "D Kelley")
+
+Footer = '<p class = "subDetails"> <a href="mailto:douglas.i.kelley@gmail.com">douglas.i.kelley@gmail.com </a> - <i> +44 (0) 7936 726 819 </i> - <a href = "douglask3.github.io"> douglask3.github.io</a></p><br>'
+###############################################################
+## Additional Sections                                       ##
+###############################################################
+
+
+AdditionalSection = '
+
+    <b> Professor Richard Washington </b>
+    School of Geography and the Environment,
+    University of Oxford
+
+    <b> 24th Feburary 2016 </b>
+
+
+Dear Professor Richard Washington
+
+My name is Douglas Kelley, and I would like to be considered for the role of “Post-doctoral Researcher in African Climate Science” as part of the REACH programme. I have recently completed a PhD in climate change ecology, were I developed a complex numerical vegetation model to explore the impacts of future climate change in another moisture controlled tropical region: Australia.
+
+Many of the climate controls and impacts are shared across Australia and East Africa: both have transitions from wet to dry tropical and sub-tropical ecosystems, where natural and agricultural productivity is particularly sensitive to available moisture (Seddon et al. 2016); both are prone to water-related natural disasters such as drought, flooding and fire (Lehmann et al. 2011; Ward et al. 2014); and both are exposed to highly uncertain but potentially devastating climate change, where changes in precipitation and increases in evaporation are likely to decrease the availability of water for human consumption, and increases in extreme precipitation events may lead to more flooding and conversely more drought (Scholze et al. 2006; Jiménez Cisneros et al. 2014; Settele et al. 2014).
+
+However, the impacts of climate change on water security in East Africa, and indeed much of the poorer regions of world, are likely to be much worse (Grey and Sadoff, 2007; Vorosmarty et al. 2010).  The 80% of the people in the region who rely directly on agriculture for their livelihoods (The Would Bank Group, 2016) have already been devistated by previous periodic droughts and floods, and are feeling the affects yet again in the current El Nino. The rapid economic development in some East African  countries (IMF, 2012) may, if unguided,  put added strain on water resources, as has happened in other parts of the world where increased urbanisation and, for some parts of society, wealth has lead to increased water demand through e.g. increased demand of energy and water-intensive agriculture (Falkenmark & Widstrand 1992), as well as decreases in the ability for environmental systems to hold and supply, through pollution and land use change.
+
+One way to mitigate possible harmful affects of climate change on water security is to continue  development “early warning systems” which allow countries to predict and prepare for future drought events or flood-prone seasons. These systems often take advantage of teleconnections with preceding climate indicators such as ocean temperatures/temperature gradients (such as ENSO, AMO, Indian Ocean Indices, shown to be particularly useful in East and Southern Africa; Mohino et al. 2011). However, these connections, and there resultant impacts, are likely to change in the future  (Müller & Roeckner 2006; Stevenson 2011; Cai et al. 2014) and changes in natural vegetation and land use (Kelley and Harrison 2014; Harrison and Kelley submitted). This is perhaps of particular concern in East Africa where, for example, the transition from drought prone North to flood prone equatorial regions during EL Nino years has already proven to be hard to predict for even the current El Nino event (Bonifacio, 2015).
+
+<NewPage>
+
+I would like the opportunity to apply skills and research experience from my time in Australia to a research team that can help develop warning and mitigation strategies, and directly influence development decisions that will help some of the world most vulnerable people adapt to climate change. I feel the REACH research program at Oxford is the ideal environment for this – the cross disciplinary setup will prove to be invaluable to teasing apart the complex nature of the impact of climate variability on water security, whilst the groups social science component and the programs partnership with Universities and institutes in Africa will help ensure research is directly applicable and relevant to the people the program is designed to help.
+
+In my “supporting statement” below, I have outlined how I think my experience in climate research makes me an ideal candidate for the role, with reference to the job description and selection criteria. Further details provided in my attached CV.  Thank you for taking the time to review my application. I look forward to hearing from you .
+
+Kind regards,
+
+Douglas Kelley'
+
+#AdditionalSection = list(Skills, Referee, ExtraCircular, Software, Training, Workshops, Research, Awards, Conferences, Publications,Qualifications,ExtendingContact, Employment)
+
+NewPage = c('Douglas Kelley', '', '<a href ="mailto:douglas.i.kelley@gmail.com"> douglas.i.kelley@gmail.com </a>')
