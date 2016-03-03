e5503988b1f47e9bd0dd1398f124cc0e828d443e
douglask3 - Reading <douglas.i.kelley@gmail.com>
2016-03-03 15:43:58 +0000

    altermning short cv for Reading and mathcing with new auto generatior

--- ReadingMetChina/cvMake.r
+++ ReadingMetChina/cvMake.r
@@ -1,15 +1,3 @@
-dir = 'ReadingMetChina'
+dir = 'ReadingMetChina/'
 post = 'Re: Research Scientist GE15229'
-
-
-shrtCVchange = paste('
-    Name[2] = "', post, '"
-    Name[3] = "CV - Summary"
-    Name[4] = "docs/agua_png_by_eross_666-d56zr6u.png"
-
-    Skills[[3]] = c(Skills[[3]][c(1,3)], "New Page", Skills[[3]][4])
-
-    AdditionalSection = list(Qualifications, Employment, Publications, Awards, Skills, Referee)
-', sep = '')
-
 source('standardApplications/cv-combine.r')
