f8182bff2a1b7315941c4861b5f6d4b03c176d44
douglask3 - Reading <douglas.i.kelley@gmail.com>
2016-04-07 11:49:02 +0100

    application for vegetation scientist, coped from Met LSM but with change in dir and with just ess des headings in ststement

---
+++ MetOfficeVegSat/Extra.CirV.r
@@ -0,0 +1,248 @@
+###############################################################
+## Cfg                                                       ##
+###############################################################
+usrID       = c('Douglas Kelley' = 'AJKyfI4AAAAJ')
+
+###############################################################
+## Head                                                      ##
+###############################################################
+Top = NULL
+
+
+###############################################################
+## Basic Infomation                                          ##
+###############################################################
+Name    = c("Douglas Kelley CV",
+            post)
+
+Contact = NULL
+
+Authors = c("DI Kelley", "D Kelley")
+###############################################################
+## Additional Sections                                       ##
+###############################################################
+ExtendingContact = list(Name = "Contact Information",
+                        c('','
+                        <table width="100%">
+                             <tr>
+                                 <td style="width:7%">
+                                     <b>Home Address:<br>
+                                        Email:<br>
+                                        Web:<br>
+                                        Phone:</b>
+                                 </td>
+                                 <td style="width:33%">
+                                     Flat 40,
+                                          18 Oxford Road,
+                                          Reading,
+                                          RG1 7LB,
+                                          UK
+                                     <br><a href="mailto:douglas.i.kelley@gmail.com">douglas.i.kelley@gmail.com</a>
+                                     <br><a href="http://douglask3.github.io/" target="_blank">douglask3.github.io</a>
+                                     <br> +44 (0) 7936 726 819
+                                 </td>
+                                 </tr>
+                                 </table>'))
+
+Qualifications = list(Name = "Academic qualifications",
+        c("PhD Climate Change Ecology" = "",
+          "2014",
+          "Macquarie University, Department of Biological Sciences, Ryde, NSW, Australia" = "https://bitbucket.org/teambcd/",
+          'Thesis: <a href="http://douglask3.github.io/docs/thesis.pdf">
+            Modelling Australian fire regimes. Available: http://douglask3.github.io/docs/thesis.pdf </a> <!--
+           DGVM model development;
+           Benchamrking modelled land surface processes;
+           Simulating vegetation-fire interactions under future climates
+          <i> avaiable at
+            <a href="http://douglask3.github.io/docs/thesis.pdf"> http://douglask3.github.io/docs/thesis.pdf </a> </i>-->'),
+
+        c("MSc Earth System Science" = "http://www.bristol.ac.uk/cabot/postgrad/msc-ccsp.html",
+          "2008",
+          "University of Bristol, Department of Earth Sciences, UK" = "http://www.bristol.ac.uk/earthsciences/",
+          ""),
+        c("BSc (Hons.) Physics" = "http://www2.warwick.ac.uk/study/undergraduate/courses/f300",
+          "2007",
+          "University of Warwick, Department of Physics, UK" = "http://www2.warwick.ac.uk/fac/sci/physics",
+          ""))
+
+Employment = list(Name = "Recent Employment History",
+
+        c("Postdoctoral Research Assistant" = "",
+          "Apr 2015-present",
+          "Department of Geography & Environmental Science, University of Reading, UK" =
+            "https://www.reading.ac.uk/geographyandenvironmentalscience/",
+          ""),
+
+        c("Research Assistant/Postdoctoral Research Assistant" = "",
+          "Jun 2014-Apr 2015",
+          "Cafe M Research Group, Department of Biological Sciences,
+                Macquarie University, Ryde, NSW, Australia" =
+                    "https://bmedlyn.wordpress.com/",
+          ""))
+
+
+Awards = list(Name = "Examples of Awards won",
+
+      c("Macquarie University Research Excellence Scholarship"="http://www.hdr.mq.edu.au/information_about/scholarships",
+        "2010-2014",
+        "Macquarie University, Ryde, NSW, Australia" = "https://www.mq.edu.au/",
+        ""),
+
+      c("Postgraduate Research Fund (PGRF)" = "http://www.hdr.mq.edu.au/information_for/current_candidates/financial_support",
+        "2013",
+        "Macquarie University, Ryde, NSW, Australia" = "https://www.mq.edu.au/",
+        ""))
+
+Workshops = list(Name = "Workshops and Consultancy Visits",
+                 c("Using Plant Functional Traits to Predict Ecosystem Vulnerability to Changing Fire Regimes" =
+                        "http://aceas.org.au/Fire_traits_report.pdf",
+                   "Australian Centre for Ecological Analysis and Synthesis (ACEAS)" = "http://www.aceas.org.au/",
+                   "Oct 2013",
+                   "University of Queensland, Brisbane, QL, Australia" =
+                        "https://www.uq.edu.au/",
+                   'Data Synthesis workshop for fire resilience and response analysis'),
+
+                 c("Fire Response Traits Database" = "https://bitbucket.org/teambcd/resprouting_workshop",
+                   "",
+                   "May 2013",
+                   "Macquarie University, Ryde, Australia" = "https://www.mq.edu.au/",
+                   'Construction of a database on the distribution of different resprouting trait types in climate space.'),
+
+                 c("Technical Assistance for Climate Change" = "https://www.researchgate.net/publication/267995882_Impacts_of_Future_Climate_Change_on_Vegetation_Fire_and_Runoff_in_Jordan",
+                   "",
+                   "Oct 2009",
+                   "Royal Society for the Conservation of Nature, Jordan" = "http://www.rscn.org.jo/",
+                   'Impacts of Future Climate Change on Vegetation, Fire, and Runoff in Jordan'))
+
+
+Training = list(Name = "Relevant Training Courses",
+        c("An Introduction to Unix for HPC and Parallel processing techniques" = "http://www.intersect.org.au/energy/training",
+          "May 2013",
+          "eMast/INTERSECT, Macquarie University, Ryde, NSW, Australia",
+          ""),
+
+        c("Programming philosophy, code structure and version control" = "http://software-carpentry.org/",
+          "Feb 2013",
+          "Software Carpentry",
+          ""),
+
+        c("Genses2Geoscience: Writing for journals"
+            = "http://www.mq.edu.au/research/research-centres,-groups-and-facilities/secure-planet/centres/centre-for-genes-to-geoscience/research-enrichment-program#writing",
+          "Aug 2012",
+          "Macquarie University, Ryde, NSW, Australia" = "https://www.mq.edu.au/",
+          ""),
+
+        c("Genses2Geoscience: Database construction using SQL"
+            = "http://www.mq.edu.au/research/research-centres,-groups-and-facilities/secure-planet/centres/centre-for-genes-to-geoscience",
+          "Sep 2011",
+          "Macquarie University, Ryde, NSW, Australia" = "https://www.mq.edu.au/",
+          ""))
+
+
+  Skills = list(Name = "Skills",
+                Main = "",
+                list(
+                   c("Programming",
+                     '<table width="100%">
+                          <tr>
+                              <td style="width:30%">
+                                  <li>Fortran</li>
+                                  <li>C++</li>
+                                  <li>C</li>
+                              </td>
+                              <td style="width:30%">
+                                  <li>R</li>
+                                  <li>Python</li>
+                                  <li>Matlab</li>
+                              </td>
+                              <td style="width:30%">
+                                  <li>Shell</li>
+                              </td>
+                          </tr>
+                      </table>',
+                      'See examples at <a href="https://github.com/douglask3" target = "_blank"> github.com/douglask3 </a>
+                      and <a href="https://bitbucket.org/douglask3/" target = "_blank"> bitbucket.org/douglask3 </a>'),
+
+                   c("Web Design & Publishing" = "",
+                       '<table width="100%">
+                            <tr>
+                                <td style="width:30%">
+                                    <li>html</li>
+                                    <li>CSS</li>
+                                    <li>PHP</li>
+                                    <li>Markdown</li>
+                                </td>
+                                <td style="width:30%">
+                                    <li>Python/R for web-page generation</li>
+                                    <li>Wordpress</li>
+                                    <li>Concrete5</li>
+                                    <li>Latex</li>
+                                </td>
+                                <td style="width:30%">
+                                    <li>Illustrator</li>
+                                    <li>Photoshop</li>
+                                    <li>GIMP</li>
+                                    <li>Scribus</li>
+                                    <li>ms/open office</li>
+                                </td>
+                            </tr>
+                        </table>',
+                      'See examples at <a href="http://douglask3.github.io/pages/websites.html" target = "_blank"> douglask3.github.io/pages/websites </a>'),
+
+                   c("Software Tool Development" = "",
+                     'I have developed several software package tools related to my research, and to help track project workflows',
+                     'See examples at <a href="http://douglask3.github.io/pages/tools.html" target = "_blank"> douglask3.github.io/pages/tools </a>')))
+
+
+ExtraCircular = list(Name = "Relevant Extra-Curricular",
+                     c('Group Discussion Chair' = "",
+                       "2011 - 2013",
+                       "Biosphere & Climate Dynamics, Department of Biological Sciences, Macquarie University, Ryde, NSW, Australia" = "http://www.mq.edu.au/about/about-the-university/faculties-and-departments/faculty-of-science-and-engineering/departments-and-centres/department-of-biological-sciences",
+                       'Organised and chaired group meetings and paper discussions'),
+
+                     c('Outreach Volunteer' = "",
+                       "2010 - 2011",
+                       "Genses2Geoscience, Department of Biological Sciences, Macquarie University, Ryde, NSW, Australia"
+                                      = "http://www.mq.edu.au/research/research-centres,-groups-and-facilities/secure-planet/centres/centre-for-genes-to-geoscience/",
+                       'Demonstrated department research during school tours of the University'),
+
+                     c('Demonstrator' = "",
+                       "2008 - 2009",
+                       "MSc Earth Systems Science and MSc natural hazards - <br> Modelling the Earth System" = "http://www.bristol.ac.uk/cabot/postgrad/msc-ccsp.html",
+                       "Conducted seminar on fire-vegetation modellings and running modelling tutorials"))
+
+Referee = list(Name = "References",
+            c(' ' = ' ',
+            '<table width="100%">
+                 <tr>
+                     <td style="width:30%">
+                         <h2><a href = "http://www.reading.ac.uk/s-p-harrison.aspx">Prof. Sandy<br>Harrison</a>
+                         </h2><small>s.p.harrison@reading.ac.uk</small>
+                     </td>
+                     <td style="width:30%">
+                         <h2><a href = "http://www.imperial.ac.uk/people/c.prentice">Prof. Colin<br>Prentice</a>
+                         </h2><small>c.prentice@imperial.ac.uk</small>
+                     </td>
+                     <td style="width:30%">
+                         <h2><a href = "https://bmedlyn.wordpress.com/">Prof. Belinda<br>Medlyn</a>
+                         </h2><small>b.medlyn@westernsydney.edu.au</small>
+                     </td>
+                 </tr>
+             </table>'),
+
+             c("",'Please see
+             <a href="http://douglask3.github.io/docs/cvMetLSM.pdf">
+             http://douglask3.github.io/docs/cvMetLSM.pdf </a> for a full academic CV, including:
+                   <li> Extended qualifications, employment, extra-curricular,
+                        awards and training history </li>
+                   <li> Skills, software development, research interests information </li>
+                   <li> Full references contact details</li>
+                   '))
+
+Footer = '<p class = "subDetails"> <b> Douglas Kelley <b> - <a href="mailto:douglas.i.kelley@gmail.com">douglas.i.kelley@gmail.com </a> - <i> +44 (0) 7936 726 819 </i> - <a href = "douglask3.github.io"> douglask3.github.io</a></p><br>'
+
+
+AdditionalSection = list(ExtendingContact, Qualifications, Employment,
+                         Skills, Awards, "New Page", Workshops,
+                         Training, ExtraCircular,
+                         Referee)
