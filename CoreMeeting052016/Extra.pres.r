70c1156c549e6c9e157c67fb2ac3fcd0b5587493
Douglas Kelley <douglas.i.kelley@gmail.com>
2016-05-25 17:14:29 +0100

    starting off bit about how important fire is

--- CoreMeeting052016/Extra.pres.r
+++ CoreMeeting052016/Extra.pres.r
@@ -3,6 +3,9 @@
 ###############################################################
 usrID       = c('Douglas Kelley' = 'AJKyfI4AAAAJ')
 Short       = TRUE
+
+makeInvisable <- function(txt) paste('<div class = "invis">', txt, '</div>')
+
 ###############################################################
 ## Basic Infomation                                          ##
 ###############################################################
@@ -30,16 +33,23 @@ Cite = c(Prentice2011 = "Prentice, IC, <b>Kelley, DI</b>, et al. (2011). Modelin
          Prentice2011b = "Prentice, IC, <b>Kelley, DI</b>, et al. (2011). Modeling fire & terrestrial carbon balance. GBC",
          Harrison2011 = "Harrison, SP, Marlon, J, Bartlein, PJ (2010). Fire in the Earth system. In Changing climates, earth systems and society",
          Kelley2014   = "<b>Kelley, DI</b> (2014). Modelling Australia Fire Regimes",
-         HarrisonSub  = 'Harrison, SP, &
-                         <b>Kelley, DI</b> (submitted). Projected changes in
+         HarrisonSub  = 'Harrison, SP, & <b>Kelley, DI</b> (submitted). Projected changes in
                          Australian fire regimes during the 21st century.',
-        Kelley2013 = "<b>Kelley, DI</b>, et al. (2013). A comprehensive
-                      benchmarking system for evaluating global
-                      vegetation models. Biogeosciences, 9(11)",
-        Kelley2014a = "<b>Kelley, DI</b>, Harrison, SP, & Prentice, IC (2014).
-                       Improved simulation of fire–vegetation interactions in the LPX-Mv1. GMD, 7(5)",
-        Kelley2014b = "<b>Kelley, DI</b>, & Harrison, SP (2014). Enhanced
-                       Australian carbon sink despite increased wildfire during the 21st century. ERL, 9(10)")
+         Kelley2013   = "<b>Kelley, DI</b>, et al. (2013). A comprehensive
+                         benchmarking system for evaluating global
+                         vegetation models. Biogeosciences, 9(11)",
+         Kelley2014a  = "<b>Kelley, DI</b>, Harrison, SP, & Prentice, IC (2014).
+                         Improved simulation of fire–vegetation interactions in the LPX-Mv1. 
+                         GMD, 7(5)",
+         Kelley2014b  = "<b>Kelley, DI</b>, & Harrison, SP (2014). Enhanced
+                         Australian carbon sink despite increased wildfire during the 
+		         21st century. ERL, 9(10)",
+	 Archibald2013= "Archibald, S, Lehmann, CER, Gomez-Dans, JL, Bradstock, RA (2014)
+                         Defining pyromes and global syndromes of fire regimes. PNAS 110(16)",
+	 Lehmann2011  = "Lehmann, CER, Archibald, S, Hoffmann, WA and Bond, WJ (2011), 
+			 Deciphering the distribution of the savanna biome. New Phyt., 191",
+         Bond         = "Bond, WJ, Woodward, FI, Midgley, GF (2005), The global distribution 
+			 of ecosystems in a world without fire. New Phyt., 165")
 
 ###############################################################
 ## Slide                                                     ##
@@ -48,8 +58,6 @@ Cite = c(Prentice2011 = "Prentice, IC, <b>Kelley, DI</b>, et al. (2011). Modelin
 ###############################################################
 ## Title                                                     ##
 ###############################################################
-
-
 
 Images.title = c(Title = 'ComputerFire.png')
 
@@ -68,11 +76,89 @@ TitlePage = list("slide",
 
 TitlePage = list(TopPage, "New Slide", "New Slide", TitlePage)
 
+# <ul class = "dashed">
+#                                    <li class = "dashed"> Find model weaknesses </li>
+#                                    <li class = "dashed"> Assess model development </li>
+#                                    <li class = "dashed"> Justify model application </li>
+# </ul>'
 
-################################################################################
-## Collate                                                                    ##
+###############################################################
+## Introduction                                              ##
+###############################################################
+# Importance of Fire
+    # World without fire
+# Where fire occurs
+# Controls on fire
+# What is fire
+    # Fire regimes
+# Vegetation adpatations (carbon)
+# Modelling fire
+    # FireMIP
+    # Simple Model
+    # Process based model
+
+###############################################################
+## Importance of fire                                        ##
+###############################################################
+# 3- 4 Mm2 area burn every year
+# 2.8 PgC/year
+Images.Importance = c(CarbonFlux1 = "CarbonFlux1.png",
+		      CarbonFlux2 = "CarbonFlux2.png")
+
+SectionTitle = "Why is fire important"
+
+List = c("Inter-annual Variablity in CO<sub>2</sub>",
+	 "Contrubutation to radiative forcing",
+	 "Trace gas emissions",
+	 "Vegetation Impacts", # World without fire
+	 "Vegetation adpatations",
+	 "Cost")
+
+ImageSlide = Problem0 = list("slide",
+            Image = list(
+                    Title = SectionTitle,
+                    SubHead = "Carbon Flux"
+                ))
+
+Importance = list("slide",
+		 Text = list(
+                    Title = SectionTitle,
+                    Text = List))
+
+Importance1 = Importance2 = Importance3 = Importanc4 = 
+	Importance5 = Importance6 = Importance
+
+Importance1[[2]]$Text[2:6] = makeInvisable(Importance1[[2]]$Text[2:6])
+Importance2[[2]]$Text[3:6] = makeInvisable(Importance2[[2]]$Text[3:6])
+Importance3[[2]]$Text[4:6] = makeInvisable(Importance3[[2]]$Text[4:6])
+Importance4[[2]]$Text[5:6] = makeInvisable(Importance4[[2]]$Text[5:6])
+Importance5[[2]]$Text[6:6] = makeInvisable(Importance5[[2]]$Text[6:6])
+
+Carbon1 = ImageSlide
+
+Carbon1[[2]]$Image  = Images.Importance['CarbonFlux1']
+Carbon1[[2]]$Footer = Cite['Kelley2014']
+
+Carbon2 = Carbon1
+Carbon2[[2]]$Image = Images.Importance['CarbonFlux2']
 
 
-Images = c(Images.index, Images.title)
 
-AdditionalSection = c(TitlePage)
+
+Importance = list(Importance1, "New Page", Carbon1, "New Slide", 
+                      Importance2, "New Slide",Carbon2, "New Slide",
+		  Importance3, "New Page", 
+		  Importance4, "New Page")
+###############################################################
+## Where						     ##
+###############################################################
+# Africa: 40% of global fire
+
+
+###############################################################
+## Collate                                                   ##
+###############################################################
+
+Images = c(Images.index, Images.title, Images.Importance)
+
+AdditionalSection = c(TitlePage, "New Slide", Importance)
