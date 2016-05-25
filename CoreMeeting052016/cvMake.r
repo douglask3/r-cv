0588a746b419cc457774512d8185f46655a5c092
Douglas Kelley <douglas.i.kelley@gmail.com>
2016-05-25 12:33:04 +0100

    copying and slighly modifyong stuff from CEH applicatoion ready for core meeting presentation. Note, this will not work well as its a copy of the ceh presnetation ut without the imagages

---
+++ CoreMeeting052016/cvMake.r
@@ -0,0 +1,7 @@
+dir = 'CoreMeeting052016/'
+post = 'UKESM core group, 26/05/16'
+
+presName = "Fire in the Earth System"
+presHead = 'Modelling Fire in the Earth System'
+
+source('standardApplications/cv-combine.r')
